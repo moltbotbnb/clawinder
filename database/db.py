e93a37dbe2c17ddaa09d70dfbdfffbeab22da028
Ubuntu <ubuntu@ip-172-31-10-192.ec2.internal>
2026-02-02 17:50:19 +0000

    fix: absolute import in db.py

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 """Database connection and session management"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from database.models import Base
 
 import os
 # Use in-memory SQLite for Railway (no persistent disk by default)
